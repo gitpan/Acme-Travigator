f6173467540e6467259932a552c0db0e4e0bcc10
Matt Monsen <mdmonsen@cpan.org>
2013-06-07 17:07:15 +0000

    Import of MDMONSEN/Acme-Travigator-0.003 from CPAN.
    gitpan-cpan-distribution: Acme-Travigator
    gitpan-cpan-version:      0.003
    gitpan-cpan-path:         MDMONSEN/Acme-Travigator-0.003.tar.gz
    gitpan-cpan-author:       MDMONSEN
    gitpan-cpan-maturity:     released

--- lib/Acme/Travigator.pm
+++ lib/Acme/Travigator.pm
@@ -3,6 +3,14 @@ package Acme::Travigator;
 use warnings;
 use strict;
 
+our (@ISA, @EXPORT_OK, %EXPORT_TAGS);
+BEGIN {
+    require Exporter;
+    @ISA         = qw(Exporter);
+    @EXPORT_OK   = qw(travigate);
+    %EXPORT_TAGS = ( all => [qw(travigate)] );
+}
+
 sub travigate {
     return "I am not sure where you want to go\n";
 }
@@ -10,6 +18,18 @@ sub travigate {
 1;
 
 # ABSTRACT: who needs google when you have the travigator
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Acme::Travigator - who needs google when you have the travigator
+
+=head1 VERSION
+
+version 0.003
 
 =head1 SYNOPSIS
 
@@ -20,3 +40,16 @@ Acme::Travigator - A new way for finding directions
 =head2 travigate
 
 return directions to a random location
+
+=head1 AUTHOR
+
+Matt Monsen <mdmonsen@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Matt Monsen.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
